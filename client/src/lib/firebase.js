a41f37343f584aa96f6d9530cffd1b34857e9abc
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-03-16 22:45:37 +0600

    updated Firebase configuration.

--- client/src/lib/firebase.js
+++ client/src/lib/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYk_U6t7MdjhzPREAayLBQ7yFxgCoBi4A",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
